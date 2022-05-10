a066b16c37aa7126c1d10d98a6cd37b67cf98fb2
Seyram Amoako <seyram12@hotmail.com>
2022-05-10 13:10:20 +0100

    message flashing working- but not for the https:// input alone..

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request, flash
 
 from application import service, app
 from application.domain.recommendations import Recommendations
@@ -13,6 +13,47 @@ from application.forms.recommendationsForm import RecommendForm
 def home():
     # return "<h1>Testing this home page route</h1>"
     return render_template("home.html")
+
+
+# @app.route('/recommendations', methods=['GET','POST'])
+# def add_recommendations():
+#     error = ""
+#     form = RecommendForm()
+#
+#     if request.method == "POST":
+#         form = RecommendForm(request.form)
+#         print(form.recommendation_name.data)
+#         recommendation_name = form.recommendation_name.data
+#         recommendation_description = form.recommendation_description.data
+#         recommendation_location = form.recommendation_location.data
+#         recommendation_website = form.recommendation_website.data
+#         recommendation_category = form.recommendation_category.data
+#         recommendation_author = form.recommendation_author.data
+#         recommendation_discount = form.recommendation_discount.data
+#
+#         if len(recommendation_name) == 0 or len(recommendation_description) == 0 or len(recommendation_location) == 0 or len(recommendation_website) == 0:
+#             error = "you're missing one of these: name, description, location and website details. Please supply."
+#         else:
+#             recommendation = Recommendations(recommendation_name=recommendation_name, recommendation_description= recommendation_description,
+#                                              recommendation_location=recommendation_location, recommendation_website=recommendation_website,
+#                                              recommendation_category=recommendation_category, recommendation_author=recommendation_author,
+#                                              recommendation_discount=recommendation_discount)
+#
+#             service.add_new_recommendations(recommendation)
+#             recommendations = service.get_all_recommendations()
+#             return render_template('list_recommendations.html', recommendations=recommendations, message=error)
+#
+#     return render_template('recommendations.html', form=form, message=error)
+
+
+# @app.route('/list', methods=['GET'])
+# def show_recomm_list():
+#     # return "<h1>Recommendations List Page</h1>"
+#     error = ""
+#     recommendations = service.get_all_recommendations()
+#     if len(recommendations) == 0:
+#         error = "There are no recommendations to display. Do add one!"
+#     return render_template("list_recommendations.html", recommendations=recommendations, message=error)
 
 
 @app.route('/recommendations', methods=['GET','POST'])
@@ -31,9 +72,23 @@ def add_recommendations():
         recommendation_author = form.recommendation_author.data
         recommendation_discount = form.recommendation_discount.data
 
-        if len(recommendation_name) == 0 or len(recommendation_description) == 0 or len(recommendation_location) == 0 or len(recommendation_website) == 0:
-            error = "you're missing one of these: name, description, location and website details. Please supply."
+        if len(recommendation_name) == 0:
+            flash("What or Where section is not filled in yet.", category="error")
+        if len(recommendation_description) == 0:
+            flash("We're missing a brief description of your experience!", category="error")
+        if len(recommendation_location) == 0:
+            flash("Please include the Location", category="error")
+        if len(recommendation_website) == 0:
+            flash("Please include a link to your recommended place website.", category="error")
+        if recommendation_website.find("https://") == -1:
+            # not working if the others boxes are entered correctly (so atm test it with another wrong box entry)
+            flash("Please include 'https:// ' in front of your website link.", category="error")
+        if len(recommendation_category) == 0:
+            flash("Please choose a Category", category="error")
+        if len(recommendation_author) == 0:
+            flash("Please provide your Name!", category="error")
         else:
+            flash("Your Recommendation has been added!", category="success")
             recommendation = Recommendations(recommendation_name=recommendation_name, recommendation_description= recommendation_description,
                                              recommendation_location=recommendation_location, recommendation_website=recommendation_website,
                                              recommendation_category=recommendation_category, recommendation_author=recommendation_author,
@@ -41,19 +96,12 @@ def add_recommendations():
 
             service.add_new_recommendations(recommendation)
             recommendations = service.get_all_recommendations()
-            return render_template('list_recommendations.html', recommendations=recommendations, message=error)
+            return render_template('list_recommendations.html', recommendations=recommendations)
 
-    return render_template('recommendations.html', form=form, message=error)
+    return render_template('recommendations.html', form=form)
+# message or category in rendertemplate()?
 
 
-# @app.route('/list', methods=['GET'])
-# def show_recomm_list():
-#     # return "<h1>Recommendations List Page</h1>"
-#     error = ""
-#     recommendations = service.get_all_recommendations()
-#     if len(recommendations) == 0:
-#         error = "There are no recommendations to display. Do add one!"
-#     return render_template("list_recommendations.html", recommendations=recommendations, message=error)
 
 
 @app.route('/contact')
